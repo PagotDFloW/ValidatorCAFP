56eaf61cf3c55dc797496d501e3f9169e98050ff
Florian <dyckeuss@outlook.fr>
2021-05-01 13:21:25 +0200

    master

--- test.php
+++ test.php
@@ -6,7 +6,7 @@ echo("c en vrac mais tkt<br>");
 // $jayson = file_get_contents("https://validator.w3.org/nu/?doc=https%3A%2F%2Fwww.w3.org%2F&out=json");
 // $tabJay = json_decode($jayson,TRUE);
 
-$url = "https://validator.w3.org/nu/?doc=https://www.amazon.fr/ref=nav_logo&out=json";
+$url = "https://jigsaw.w3.org/css-validator/validator?uri=http%3A%2F%2Fwww.w3.org%2F&warning=0&profile=css2&output=json";
 
 // $options = array(
 //   'http'=>array(
@@ -50,6 +50,22 @@ $resJson = json_decode($response, true);
 print_r($resJson);
 echo "-----------------------------------------------------------<br>";
 echo $resJson["messages"][5]["type"];
+//-------------------------CSS API---------------------------------
+echo"------------------------------------------------------------------------------------------------------</br>";
+echo "<h2>TEST CSS API</br></h2>";
+$options = array(
+  'http'=>array(
+    'method'=>"GET",
+    'header'=>"Accept-language: en\r\n" .
+              "Cookie: foo=bar\r\n" .  // check function.stream-context-create on php.net
+              "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.114 Safari/537.36\r\n" // i.e. An iPad 
+  )
+);
 
+$context = stream_context_create($options);
+$file = file_get_contents($url, false, $context);
+$tabJay = json_decode($file,TRUE);
+
+var_dump($tabJay);
 
 ?>
